667c42be6753535375d69f5afa5e64a1877efe15
Arthur <arthur1472@gmai.com>
2020-02-28 03:33:41 +0100

    Seeder fix

--- database/seeds/StatusTableSeeder.php
+++ database/seeds/StatusTableSeeder.php
@@ -12,11 +12,11 @@ class StatusTableSeeder extends Seeder
     public function run()
     {
         \App\Status::insert([
-            ['id' => 1, 'name' => 'Onafgerond'],
-            ['id' => 2, 'name' => 'Nieuw'],
-            ['id' => 3, 'name' => 'In behandeling'],
-            ['id' => 4, 'name' => 'Klaar'],
-            ['id' => 5, 'name' => 'Afgewezen'],
+            ['id' => 1, 'name' => 'Onafgerond', 'button_class' => 'btn-secondary'],
+            ['id' => 2, 'name' => 'Nieuw', 'button_class' => 'btn-success'],
+            ['id' => 3, 'name' => 'In behandeling', 'button_class' => 'btn-primary'],
+            ['id' => 4, 'name' => 'Klaar', 'button_class' => 'btn-success'],
+            ['id' => 5, 'name' => 'Afgewezen', 'button_class' => 'btn-danger'],
 //            ['id' => 6, 'name' => 'Nieuw'],
 //            ['id' => 7, 'name' => 'Nieuw'],
         ]);
