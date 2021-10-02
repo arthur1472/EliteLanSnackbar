33261c7111b115aeed64662b3ecf004e4d390c74
Arthur <arthur1472@gmail.com>
2021-10-02 18:43:12 +0200

    Clicking on an order now gives a list of the items

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -26,29 +26,62 @@
                                     </th>
                                 </tr>
                                 </thead>
-                                <tbody class="bg-white divide-y divide-gray-200">
                                 @foreach($orders as $order)
-                                    <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                            {{$order->id}}
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                            {{$order->user->name}}
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                            <div class="inline-flex items-center font-bold px-3 py-2 border border-transparent text-sm text-white leading-4 font-medium rounded-md {{$order->status->color_class}}">
-                                                {{$order->status->name}}
-                                            </div>
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                            {{$order->created_at->toDateTimeString()}}
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex justify-end">
-                                            <a href="{{route('admin.orders.show', ['order' => $order])}}" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Bekijk</a>
-                                        </td>
-                                    </tr>
+                                    <tbody x-data="{ open: false }" class="bg-white divide-y divide-gray-200">
+                                        <tr @click="open = !open">
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                                {{$order->id}}
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                {{$order->user->name}}
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                <div class="inline-flex items-center font-bold px-3 py-2 border border-transparent text-sm text-white leading-4 font-medium rounded-md {{$order->status->color_class}}">
+                                                    {{$order->status->name}}
+                                                </div>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                {{$order->created_at->toDateTimeString()}}
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex justify-end">
+                                                <a href="{{route('admin.orders.show', ['order' => $order])}}" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Bekijk</a>
+                                            </td>
+                                        </tr>
+                                        <tr x-show="open">
+                                            <td>
+                                                <ul>
+                                                    @foreach($order->orderLines as $orderLine)
+                                                        <li class="p-2 sm:p-2">
+                                                            <div class="flex items-center sm:items-start">
+                                                                <div class="flex-1 text-sm">
+                                                                    <div class="font-medium text-gray-900 sm:flex sm:justify-between">
+                                                                        <h5>
+                                                                            {{$orderLine->item->name}}
+                                                                        </h5>
+                                                                        <p class="mt-2 text-md font-bold text-gray-900 sm:mt-0">
+                                                                            {{$orderLine->quantity}}x
+                                                                        </p>
+                                                                    </div>
+                                                                    @if($orderLine->toppingModels)
+                                                                        <p class="block mt-2">
+                                                                        <div class="mt-2">
+                                                                            @foreach($orderLine->toppingModels as $topping)
+                                                                                <p class="mt-1 text-md text-gray-700">
+                                                                                    - {{$topping->name}}
+                                                                                </p>
+                                                                            @endforeach
+                                                                        </div>
+                                                                        </p>
+                                                                    @endif
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
+                                            </td>
+                                        </tr>
+                                    </tbody>
                                 @endforeach
-                                </tbody>
                             </table>
                         </div>
                     </div>
