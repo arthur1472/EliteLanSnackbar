80cf69d914c9d84304c644a2450c24c1158370b2
Shift <shift@laravelshift.com>
2023-02-09 20:17:28 +0000

    Adopt anonymous migrations

--- database/migrations/2021_09_22_220611_create_cart_lines_table.php
+++ database/migrations/2021_09_22_220611_create_cart_lines_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCartLinesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -32,4 +32,4 @@ class CreateCartLinesTable extends Migration
     {
         Schema::dropIfExists('cart_lines');
     }
-}
+};
